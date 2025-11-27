cec57cea49ad98f69f8f9d6f6c2628c52d1c0c73
rishii2208 <rishirajprajapati22@gmail.com>
2025-11-27 20:49:08 +0530

    new archove for tectonic

--- server/render-build.sh
+++ server/render-build.sh
@@ -2,11 +2,13 @@
 set -euo pipefail
 
 TEX_VERSION=0.15.0
-ARCHIVE="tectonic-${TEX_VERSION}-x86_64-unknown-linux-gnu.tar.gz"
+BUILD_TAG=20251006
+ARCHIVE="tectonic-${TEX_VERSION}+${BUILD_TAG}-x86_64-unknown-linux-gnu.tar.gz"
 URL="https://github.com/tectonic-typesetting/tectonic/releases/download/tectonic-${TEX_VERSION}/${ARCHIVE}"
 
 mkdir -p bin tmp/tectonic-download
-curl -L "$URL" | tar xz -C tmp/tectonic-download
+curl -fL "$URL" -o tmp/tectonic-download/${ARCHIVE}
+tar -xzf tmp/tectonic-download/${ARCHIVE} -C tmp/tectonic-download
 mv tmp/tectonic-download/tectonic*/tectonic bin/tectonic
 chmod +x bin/tectonic
 rm -rf tmp/tectonic-download
